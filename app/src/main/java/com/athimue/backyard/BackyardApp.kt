3dcf9aa5c4f091cf0c11b8415c6a22c37f81dbc9
athimue <84283744+athimue@users.noreply.github.com>
2026-03-10 20:53:06 +0100

    feat: add app entry points
    Add AndroidManifest, Hilt Application class (BackyardApp) and MainActivity
    as the root host for Jetpack Compose navigation.

---
+++ app/src/main/java/com/athimue/backyard/BackyardApp.kt
@@ -0,0 +1,8 @@
+package com.athimue.backyard
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BackyardApp : Application()
+
---
+++ app/src/main/java/com/athimue/backyard/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.athimue.backyard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.athimue.backyard.composable.BackyardNavHost
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            // It's best practice to wrap your content in the theme
+            BackyardNavHost()
+        }
+    }
+}
