9fc0b44e2552a8ec03c336e91aa55842b7a780b7
athimue <84283744+athimue@users.noreply.github.com>
2026-03-10 20:52:45 +0100

    feat: add UI theme and app resources
    Add AppColors and AppTypography theme definitions, color/string/theme XML
    resource files, drawables and launcher icons.

---
+++ app/src/main/java/com/athimue/backyard/ui/theme/AppColors.kt
@@ -0,0 +1,22 @@
+package com.athimue.backyard.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+object AppColors {
+    val Orange = Color(0xFFFF9800)
+    val OrangeDim = Color(0xFFE65100)
+    val OrangeSubtle = Color(0x33FF9800)
+    val Yellow = Color(0xFFFFC107)
+    val White = Color.White
+    val WhiteDim = Color(0xCCFFFFFF)
+    val Black = Color(0xFF000000)
+    val SurfaceDark = Color(0xFF1A1A1A)
+    val SurfaceMid = Color(0xFF2E2E2E)
+    val SurfaceCell = Color(0xFF242424)
+    val Gray = Color(0xFF757575)
+    val GraySubtle = Color(0xFF424242)
+    val GreenFilled = Color(0xFF1B5E20)
+    val GreenFilledBorder = Color(0xFF43A047)
+    val RedFilled = Color(0xFF5D1A1A)
+    val RedFilledBorder = Color(0xFFEF5350)
+}
---
+++ app/src/main/java/com/athimue/backyard/ui/theme/AppTypography.kt
@@ -0,0 +1,23 @@
+package com.athimue.backyard.ui.theme
+
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.sp
+
+object AppTypography {
+    val fontFamily = FontFamily.Monospace
+
+    val titleSize: TextUnit = 52.sp
+    val subtitleSize: TextUnit = 28.sp
+    val bodyLargeSize: TextUnit = 36.sp
+    val bodyMediumSize: TextUnit = 24.sp
+    val bodySmallSize: TextUnit = 18.sp
+    val labelSize: TextUnit = 14.sp
+    val clockSize: TextUnit = 80.sp
+    val chronoSize: TextUnit = 72.sp
+
+    val bold = FontWeight.Bold
+    val semiBold = FontWeight.SemiBold
+    val normal = FontWeight.Normal
+}
