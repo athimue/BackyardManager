40d8434bac0c8ee7c80bd2727a4a43db8f426b59
athimue <84283744+athimue@users.noreply.github.com>
2026-05-31 23:14:41 +0200

    avoid application to put in pause

--- app/src/main/java/com/athimue/backyard/MainActivity.kt
+++ app/src/main/java/com/athimue/backyard/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.athimue.backyard
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.athimue.backyard.feature.countdown.api.CountdownFeatureApi
@@ -28,6 +29,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
 
         setContent {
             BackyardNavHost(
